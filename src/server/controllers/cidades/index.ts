5fa0c9c38ce1e543f6852ed08d3dd2914318c044
kelver-web <kelverwt@gmail.com>
2023-10-04 19:36:51 -0300

    feat: finalization methods http

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,8 +1,14 @@
+import * as getById from './GetById'
 import * as create from './Create'
 import * as getAll from './GetAll'
+import * as updateById from './UpdateById'
+import * as deleteById from './DeleteById'
 
 
 export const CidadesController = {
     ...create,
     ...getAll,
+    ...getById,
+    ...updateById,
+    ...deleteById,
 }
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,9 +4,7 @@ import { Router } from 'express'
 import { CidadesController } from './../controllers'
 
 
-
 const router = Router()
-
 
 
 router.get('/', (_, res) => {
@@ -14,6 +12,9 @@ router.get('/', (_, res) => {
 })
 
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll)
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById)
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById)
+router.delete('/cidades/:id', CidadesController.deleteValidation, CidadesController.deleteById)
 router.post('/cidades', CidadesController.createValidation, CidadesController.create)
 
 
